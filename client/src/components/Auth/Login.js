ed5777673cda0e0e1f01ad67a63c821fe3f5aa8c
David Hernquist <david_hernquist1@gmail.com>
2019-07-27 21:05:18 -0400

    finished with apollo

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,36 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GraphQLClient } from "graphql-request";
 import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
 
-const ME_QUERY = `
-  {
-    me {
-      _id
-      name
-      picture
-      email
-    }
-  }
-`;
+import Context from "../../context";
+import { ME_QUERY } from "../../graphql/queries";
+import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context);
+
   const onSuccess = async googleUser => {
-    const idToken = googleUser.getAuthResponse().id_token;
-    console.log("idToken:", idToken);
-    const client = new GraphQLClient("http://localhost:4000/playground", {
-      headers: { authorization: idToken }
-    });
-    const data = await client.request(ME_QUERY);
-    console.log("data", { data });
+    try {
+      const idToken = googleUser.getAuthResponse().id_token;
+      const client = new GraphQLClient("http://localhost:4000/playground", {
+        headers: { authorization: idToken }
+      });
+      const { me } = await client.request(ME_QUERY);
+
+      dispatch({ type: "LOGIN_USER", payload: me });
+    } catch (err) {
+      onFailure(err);
+    }
   };
 
   const clientId = process.env.REACT_APP_OAUTH_CLIENT_ID;
-  console.log(clientId);
+
+  const onFailure = err => console.error("Error logging in", err);
 
   return (
-    <GoogleLogin clientId={clientId} onSuccess={onSuccess} isSignedIn={true} />
+    <div className={classes.root}>
+      <Typography
+        component="h1"
+        variant="h3"
+        gutterBottom
+        noWrap
+        style={{ color: "rgb(66, 133, 244)" }}
+      >
+        Welcome
+      </Typography>
+      <GoogleLogin
+        theme="dark"
+        clientId={clientId}
+        onSuccess={onSuccess}
+        isSignedIn={true}
+        onFailure={onFailure}
+      />
+    </div>
   );
 };
 
---
+++ client/src/graphql/queries.js
@@ -0,0 +1,12 @@
+const ME_QUERY = `
+  {
+    me {
+      _id
+      name
+      picture
+      email
+    }
+  }
+`;
+
+export { ME_QUERY };
