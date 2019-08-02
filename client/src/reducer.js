7d9d683febd65c221b2c612a37730ac421e8e2c4
David Hernquist <david_hernquist1@gmail.com>
2019-08-02 14:35:15 -0400

    updating for deployment

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -45,6 +45,7 @@ export default function reducer(state, { type, payload }) {
     case "CREATE_PIN":
       const newPin = payload;
       const prevPins = state.pins.filter(pin => pin._id !== newPin._id);
+
       return {
         ...state,
         pins: [...prevPins, newPin]
@@ -58,11 +59,21 @@ export default function reducer(state, { type, payload }) {
     case "DELETE_PIN":
       const deletedPin = payload;
       const filteredPins = state.pins.filter(pin => pin._id !== deletedPin._id);
+      if (state.currentPin) {
+        const isCurrentPin = deletedPin._id === state.currentPin._id;
+        if (isCurrentPin) {
+          return {
+            ...state,
+            pins: filteredPins,
+            currentPin: null
+          };
+        }
+      }
       return {
         ...state,
-        pins: filteredPins,
-        currentPin: null
+        pins: filteredPins
       };
+
     case "CREATE_COMMENT":
       const updatedCurrentPin = payload;
       const updatedPins = state.pins.map(pin =>
